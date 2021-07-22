31e975ab2cdd33dcf6d85cd6ebd4132afa1b4d73
Evandro Pereira Dias <ev047953@intelbras.com.br>
2021-07-22 17:00:05 -0300

    altera pasta

---
+++ Curso_prime/1_operadores.php
@@ -0,0 +1,25 @@
+<?php
+$x = 7;
+$y = 10;
+$z;
+$a = "Programação ";
+$b = "PHP";
+
+//realizando operações.
+$z = $x + $y;
+echo "Soma = $z \n";
+
+$z = $x - $y;
+echo "Subtração = $z \n";
+
+$z = $x * $y;
+echo "Multiplicação = $z \n";
+
+$z = $x / $y;
+echo "Divisão = $z \n";
+
+$z = $x % $y;
+echo "Módulo = $z \n";
+
+echo "$a . $b"; //concatenar
+?>
---
+++ Curso_prime/2_ifElse.php
@@ -0,0 +1,16 @@
+<?php
+
+$dia = 2;
+
+if ($dia == 7) {
+    echo "<p>O dia é $dia</p>";
+} else {
+    echo "<p> O dia não é 7 <p>";
+} 
+
+if ($dia % 2 == 0) {
+    echo "<p>dia = $dia então é par <p>";
+} else {
+    echo "<p>dia = $dia então é impar</p>";
+}
+?>
